bc28ee38eddd871f69ebcc2ccfc695e6b5016eef
dor_sax <github@dorsax.de>
2022-01-15 19:12:05 +0100

    add cache

--- scrapesupport.py
+++ scrapesupport.py
@@ -4,15 +4,46 @@ import os
 import logging
 import requests
 import re
+from pathlib import Path
+
+
 
 logging.basicConfig(level=logging.INFO)
 
+# read the config
 client = discord.Client()
-source_folder=os.path.dirname(os.path.realpath(__file__))+os.path.sep
-configstream = open(source_folder+"config.yml", 'r')
+source_folder = Path(__file__).parent
+with open(source_folder / "config.yml", 'rb') as configstream:
 config = yaml.safe_load (configstream)
 token = config.get('token')
 pastebin_key = config.get('pastebin_key')
+cache_folder = source_folder / 'cache'
+
+async def save_to_cache(attachment):
+    local_file = cache_folder / (str(uuid4())+".txt")
+    try:
+        await attachment.save(local_file)
+        logging.info(attachment.filename+' was saved!')
+        local_file_content = ""
+        with open(local_file, 'rb') as local_file_stream:
+            local_file_content = local_file_stream.read()
+        try:
+            local_file.unlink()
+        except:
+            logging.error(attachment.filename+' could not be deleted.')
+        finally:
+            return local_file_content
+    except: 
+        logging.error(attachment.filename+' could not be saved.')
+
+    
+    return ""
+
+def clear_cache():
+    cache_folder.rmdir
+    cache_folder.mkdir(parents=True, exist_ok=True)
+    return True
+
 
 @client.event
 async def on_ready():
@@ -26,20 +57,10 @@ async def on_message(message):
     if len(message.attachments)>0:
         for attachment in message.attachments:
             if attachment.content_type.startswith('text'):
-                was_error = False
-                local_file=source_folder +attachment.filename
-                try:
-                    await attachment.save(local_file)
-                    logging.info(attachment.filename+' was saved!')
-                except: 
-                    logging.error(attachment.filename+' could not be saved.')
-                    was_error = True
-                
-                if not(was_error):
-                    with open(local_file, 'rb') as local_file_stream:
-
+                attachment_content = await save_to_cache(attachment)
+                if attachment_content!="":
                         post_data = {
-                            "api_paste_code" : local_file_stream.read(),
+                            "api_paste_code" : attachment_content,
                             "api_option" : "paste",
                             "api_dev_key" : pastebin_key,
                             "api_paste_name" : attachment.filename
@@ -50,10 +71,7 @@ async def on_message(message):
                         logging.info ('api response: '+str(response_content))
                         if (response_content.startswith('https://pastebin.com')):
                             await message.channel.send('For better support on mobile, your file was uploaded to '+response_content)
-                
-        # await message.channel.send('Message with attachments detected!')
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        return
 
-
+clear_cache()
 client.run(token)
